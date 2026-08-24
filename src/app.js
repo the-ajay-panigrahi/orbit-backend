8edc4316f1b34fa65e2b668a742bf4577ff64225
Ajay Panigrahi <ajaybpanigrahi@gmail.com>
2026-08-24 17:20:09 +0530

    feat: add sub-routes for /sample and home user paths and reorganize root route handlers

--- src/app.js
+++ src/app.js
@@ -4,21 +4,38 @@ const app = express()
 const PORT = 7777
 
 
-app.use("/home", (req, res) => {
-    res.send("Ayodhya!!!")
-})
 
-app.use("/lanka", (req, res) => {
-    res.send("Lanka dahan!!!!!!!")
-})
 
 app.use("/sample", (req, res) => {
     res.send("Sample Testing 🥳")
 })
 
+
+app.use("/home/user2/234/51", (req, res) => {
+    res.send("Home sub route 51")
+})
+
+
+app.use("/home/user2/234/52", (req, res) => {
+    res.send("Home sub route 52")
+})
+
+app.use("/home", (req, res) => {
+    res.send("Ayodhya!!!")
+})
+
+
+app.use("/lanka", (req, res) => {
+    res.send("Lanka dahan!!!!!!!")
+})
+
 app.use("/", (req, res) => {
     res.send("Ram Ram!")
 })
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is successfully running on port ${PORT}`);
