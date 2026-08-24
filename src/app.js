0573899297f14c14064100d57c24647f3830a326
Ajay Panigrahi <ajaybpanigrahi@gmail.com>
2026-08-24 18:44:08 +0530

    refactor: comment out deprecated routes and add fly pattern matching endpoint

--- src/app.js
+++ src/app.js
@@ -3,23 +3,23 @@ const express = require("express")
 const app = express()
 const PORT = 7777
 
-app.get("/abc/:userId/:password/:name", (req, res) => {
-    console.log(req.params);
-    res.end()
-})
+// app.get("/abc/:userId/:password/:name", (req, res) => {
+//     console.log(req.params);
+//     res.end()
+// })
 
-app.get("/abc", (req, res) => {
-    console.log(req.query.lastName);
-    res.end()
-})
+// app.get("/abc", (req, res) => {
+//     console.log(req.query.lastName);
+//     res.end()
+// })
 
-app.get(/ab?c/, (req, res) => {
+app.get(/.*fly$/, (req, res) => {
     res.send("Working Dude!")
 })
 
-app.get(/a(b)+c/, (req, res) => {
-    res.send("Working Dude!")
-})
+// app.get(/a(b)+c/, (req, res) => {
+//     res.send("Working Dude!")
+// })
 
 
 
