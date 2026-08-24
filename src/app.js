68606e110edc74bb2d8591f933c17b674bf8dae7
Ajay Panigrahi <ajaybpanigrahi@gmail.com>
2026-08-24 18:26:31 +0530

    refactor: remove legacy user routes and implement dynamic pattern-based test routes

--- src/app.js
+++ src/app.js
@@ -3,67 +3,23 @@ const express = require("express")
 const app = express()
 const PORT = 7777
 
-
-
-
-app.get("/user", (req, res) => {
-    res.send({ firstName: "Ajay", lastName: "Panigrahi" })
+app.get("/abc/:userId/:password/:name", (req, res) => {
+    console.log(req.params);
+    res.end()
 })
 
-app.post("/user", (req, res) => {
-    console.log("Data is successfully pushed!");
-    res.send({ firstName: "Vijay", lastName: "Panigrahi" })
+app.get("/abc", (req, res) => {
+    console.log(req.query.lastName);
+    res.end()
 })
 
-app.put("/user", (req, res) => {
-    console.log("Replaced/created");
-    res.send({ firstName: "Dhananjaya", lastName: "Panigrahi" })
+app.get(/ab?c/, (req, res) => {
+    res.send("Working Dude!")
 })
 
-
-app.use("/user", (req, res) => {
-    res.send("HehhhhaHahahahahah!!!!!!!!")
+app.get(/a(b)+c/, (req, res) => {
+    res.send("Working Dude!")
 })
-
-
-app.patch("/user", (req, res) => {
-    console.log("Sample update/bug fix");
-    res.send({ firstName: "Dhananjaya", lastName: "Panigrahi" })
-})
-
-app.delete("/user", (req, res) => {
-    console.log("DELETED!!");
-    res.send(123)
-})
-
-app.use("/sample", (req, res) => {
-    res.send("Sample Testing 🥳")
-})
-
-
-app.use("/home/user2/234/51", (req, res) => {
-    res.send("Home sub route 51")
-})
-
-
-app.use("/home/user2/234/52", (req, res) => {
-    res.send("Home sub route 52")
-})
-
-app.use("/home", (req, res) => {
-    res.send("Ayodhya!!!")
-})
-
-
-app.use("/lanka", (req, res) => {
-    res.send("Lanka dahan!!!!!!!")
-})
-
-// app.use("/", (req, res) => {
-//     res.send("Ram Ram!")
-// })
-
-
 
 
 
