5b729ec3fda9050fdfb24bf573bf3e2c98d6527e
Ajay Panigrahi <ajaybpanigrahi@gmail.com>
2026-08-28 14:18:15 +0530

    feat(user): implement CRUD endpoints and add schema options

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ const User = require("./models/user");
 const app = express();
 const PORT = process.env.PORT || 7777;
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
+  console.log(req.body);
+
   try {
-    const userObject = {
-      firstName: "Ram",
-      lastName: "Kapoor",
-    };
+    const userObject = req.body;
 
     const user = new User(userObject);
 
@@ -23,6 +24,57 @@ app.post("/signup", async (req, res) => {
       error.message,
     );
     res.status(400).send("Signup failed!!");
+  }
+});
+
+// get one user from the db
+app.get("/user", async (req, res) => {
+  console.log(req.body.lastName);
+
+  // const user = await User.findById(req.body.myId);
+  const user = await User.findOne({ lastName: req.body.lastName });
+  console.log(user);
+
+  res.send(user);
+});
+
+// Feed-api, get all users from the db
+app.get("/feed", async (req, res) => {
+  const users = await User.find({});
+  console.log(users);
+
+  res.send(users);
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.myId);
+    res.send("user successfully deleted!");
+  } catch (error) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { lastName: req.body.lastName },
+      req.body,
+      {
+        returnDocument: "after",
+      },
+    );
+    console.log(user);
+
+    if (!user) {
+      res.status(400).send("No user found!");
+    } else {
+      res.send("user successfully updated");
+    }
+  } catch (error) {
+    console.log(error.message);
+
+    res.status(400).send("Something went wrong!");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-});
+  { strict: true, timestamps: true },
+);
 
 const User = mongoose.model("User", userSchema);
 
